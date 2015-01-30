126f644ab01744c417a98ae9bb02856f4343330f
Russell Hutson <russell.hutson@exertismicro-p.co.uk>
2015-01-30 11:52:54 +0000

    Re-hosting as 7coders have disappeared from GitHub

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # yii2-handlebars
 Fork of https://github.com/7coders/yii2-handlebars which disappeared mysteriously from GitHub
+
+From the original 7coders README.md
+==================================
+
+Handlebars renderer for Yii2 framework.
+
+This extension provides a `ViewRender` that would allow you to use handlebars-php view template engine.
+
+To use this extension, simply add the following code in your application configuration:
+
+```php
+return [
+    //....
+    'components' => [
+        'view' => [
+            'renderers' => [
+                'handlebars' => [
+                    'class' => 'sevencoders\handlebars\ViewRenderer',
+                    // the file extension of Handlebars templates
+                    // 'extension' => '.handlebars',
+                    // path alias pointing to where Handlebars cache will be stored. Set to false to disable templates cache.
+                    // 'cache' => '@app/runtime/handlebars',
+                    // array helpers to preload, can contain class names (strings).
+                    // If empty - only default helpers will be preloaded
+                    // 'helpers' => [],
+                    // a callable function to escape values
+                    // 'escape' => 'htmlspecialchars',
+                    // array to pass as extra parameter to the escape function
+                    // 'escapeArgs' => [ENT_COMPAT, 'UTF-8']
+                ],
+            ],
+        ],
+    ],
+];
+```
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist 7coders/yii2-handlebars "*"
+```
+
+or add
+
+```
+"yiisoft/yii2-twig": "*"
+```
+
+to the require section of your composer.json.
+
---
+++ ViewRenderer.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * @link
+ * @copyright Andriy Kravchenko
+ * @license
+ */
+
+namespace sevencoders\handlebars;
+
+use Yii;
+use yii\base\View;
+use yii\base\ViewRenderer as BaseViewRenderer;
+use Handlebars;
+
+/**
+ * HandlebarsViewRenderer allows you to use Handlebar templates in views.
+ *
+ * @property
+ *
+ * @author
+ * @since
+ */
+class ViewRenderer extends BaseViewRenderer
+{
+
+    /**
+     * @var \Handlebars\Handlebars
+     */
+    public $handlebars;
+
+
+    /**
+     * @var string path alias pointing to where Handlebars cache will be stored. Set to false to disable
+     * templates cache.
+     */
+    public $cache = '@app/runtime/handlebars';
+
+    /**
+     * @var array helpers to preload, can contain class names (strings).
+     * If empty - only default helpers will be preloaded
+     */
+    public $helpers;
+
+    /**
+     * @var string a callable function to escape values
+     */
+    public $escape = 'htmlspecialchars';
+
+    /**
+     * @var array to pass as extra parameter to the escape function
+     */
+    public $escapeArgs = [
+        ENT_COMPAT,
+        'UTF-8'
+    ];
+
+    /**
+     * @var string the file extension of Handlebars templates
+     */
+    public $extension = ".handlebars";
+
+    public function init()
+    {
+        $options = [];
+
+        if (!empty($this->escape)) {
+            $options['escape'] = $this->escape;
+            if (!empty($this->escapeArgs)) {
+                $options['escapeArgs'] = $this->escapeArgs;
+            }
+        }
+
+        if (!empty($this->helpers)) {
+            foreach ($this->helpers as $helper) {
+                $options['helpers'][] = new $helper;
+            }
+        }
+
+        if (!empty($this->cache)) {
+            $options['cache'] = new \Handlebars\Cache\Disk(Yii::getAlias($this->cache));
+        }
+
+        $this->handlebars = new \Handlebars\Handlebars($options);
+    }
+
+    /**
+     * Renders a view file.
+     *
+     * This method is invoked by [[View]] whenever it tries to render a view.
+     * Child classes must implement this method to render the given view file.
+     *
+     * @param View $view the view object used for rendering the file.
+     * @param string $file the view file.
+     * @param array $params the parameters to be passed to the view file.
+     *
+     * @return string the rendering result
+     */
+    public function render($view, $file, $params)
+    {
+        $this->handlebars->setLoader(new Handlebars\Loader\FilesystemLoader(dirname($file), ['extension' => $this->extension]));
+        return $this->handlebars->render(pathinfo($file, PATHINFO_BASENAME), $params);
+    }
+}
